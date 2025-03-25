c894c6a37df42451bf30fdcadb39cc686fbbf322
Nilakshana2002 <5657pasindu@gmail.com>
2025-03-25 22:59:18 +0530

    course model & departmrnt model

---
+++ Models/Course.java
@@ -0,0 +1,37 @@
+package Models;
+
+public class Course {
+    private int courseId;
+    private String courseName;
+    private String courseType;
+    private int credit;
+    private int departmentId;
+
+//    constructors,getters
+
+
+    public Course() {
+    }
+
+    public Course(int courseId, String courseName, String courseType, int credit, int departmentId) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.courseType = courseType;
+        this.credit = credit;
+        this.departmentId = departmentId;
+    }
+
+    public int getCourseId(){
+        return courseId;
+    }
+
+    public String getCourseName(){
+        return courseName;
+    }
+
+    public String getCourseType(){
+        return courseType;
+    }
+
+
+}
---
+++ Models/Department.java
@@ -0,0 +1,24 @@
+package Models;
+
+public class Department {
+    private int departmentId;
+    private String departmentName;
+
+    public Department() {
+    }
+
+    public Department(int departmentId, String departmentName) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+    }
+
+    public int getDepartmentId(){
+        return departmentId;
+
+    }
+
+    public String getDepartmentName(){
+        return departmentName;
+    }
+}
+
