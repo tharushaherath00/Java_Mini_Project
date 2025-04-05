6dddccd142a8c2237849494138c4cf4addc17836
Ashen Chathuranga <ktauchathuranga@gmail.com>
2025-04-05 06:09:34 +0530

    fxing bat password

--- db/import.bat
+++ db/import.bat
@@ -3,7 +3,7 @@ echo Starting MySQL database import...
 
 set MYSQL_PATH="C:\Program Files\MySQL\MySQL Server 8.0\bin"
 set USER=root
-set PASSWORD=Nilakshana_123@
+set PASSWORD=root
 set SQL_FILE="sample.sql"
 
 if not exist %SQL_FILE% (
